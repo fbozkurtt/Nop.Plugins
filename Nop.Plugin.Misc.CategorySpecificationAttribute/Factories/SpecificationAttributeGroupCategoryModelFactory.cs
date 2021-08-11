d23aa3af5b3b6805369a768500142bf06360bf81
fbozkurtt@mobile <furkanbozkurt55@gmail.com>
2021-08-11 22:24:59 +0300

    Add selected categories to model.

--- Nop.Plugin.Misc.CategorySpecificationAttribute/Factories/SpecificationAttributeGroupCategoryModelFactory.cs
+++ Nop.Plugin.Misc.CategorySpecificationAttribute/Factories/SpecificationAttributeGroupCategoryModelFactory.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Plugin.Misc.CategorySpecificationAttribute.Models;
+﻿using Nop.Plugin.Misc.CategorySpecAttribute.Services;
+using Nop.Plugin.Misc.CategorySpecificationAttribute.Models;
 using Nop.Web.Areas.Admin.Factories;
 using System;
 using System.Collections.Generic;
@@ -11,14 +12,19 @@ namespace Nop.Plugin.Misc.CategorySpecificationAttribute.Factories
     public class SpecificationAttributeGroupCategoryModelFactory : ISpecificationAttributeGroupCategoryModelFactory
     {
         private readonly IBaseAdminModelFactory _baseAdminModelFactory;
+        private readonly ICategorySpecificationAttributeService _categorySpecificationAttributeService;
 
-        public SpecificationAttributeGroupCategoryModelFactory(IBaseAdminModelFactory baseAdminModelFactory)
+        public SpecificationAttributeGroupCategoryModelFactory(IBaseAdminModelFactory baseAdminModelFactory, ICategorySpecificationAttributeService categorySpecificationAttributeService)
         {
             _baseAdminModelFactory = baseAdminModelFactory;
+            _categorySpecificationAttributeService = categorySpecificationAttributeService;
         }
 
         public async Task<SpecificationAttributeGroupCategoryModel> PrepareSpecificationAttributeGroupCategoryModelAsync(SpecificationAttributeGroupCategoryModel model)
         {
+            model.SelectedCategoryIds = (await _categorySpecificationAttributeService.GetBySpecificationAttributeGroupId(model.Id))
+                        .Select(c => c.CategoryId).ToList();
+
             await _baseAdminModelFactory.PrepareCategoriesAsync(model.AvailableCategories, false);
             foreach (var categoryItem in model.AvailableCategories)
             {
